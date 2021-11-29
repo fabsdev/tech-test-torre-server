d5b5ce8617ea5fea204255af84d01348f716c877
Fabián Barreto <fabiandresbarreto@gmail.com>
2021-11-29 15:16:46 -0500

    Principal route API
    * New API route
    --> /api/$username = get data from a user in Torre
    
    * New folders
    --> routes (api.js)
    --> controllers (user.js)

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const cors = require("cors");
 
 //Express Server
 const app = express();
+const apiRouter = require("./src/routes/api");
 
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api", apiRouter);
 
 //First route
 app.get("/", (req, res) => {
---
+++ src/controllers/user.js
@@ -0,0 +1,30 @@
+const fetch = require("node-fetch");
+
+const getUser = async (req, res) => {
+  try {
+    const username = req.params.username;
+    let user = {};
+    await fetch(`https://torre.bio/api/bios/${username}`)
+      .then((promise) => promise.json())
+      .then((data) => {
+        user = {
+          name: data.person.name,
+          picture: data.person.picture,
+          profession: data.person.professionalHeadline,
+          location: data.person.location,
+          strengths: data.strengths,
+        };
+      });
+    res.json({
+      ok: true,
+      user,
+    });
+  } catch (error) {
+    res.status(404).json({
+      ok: false,
+      msg: "User does not exist.",
+    });
+  }
+};
+
+module.exports = { getUser };
